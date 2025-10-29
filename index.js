dd8d2dbca6552e2f1ef9f420288428b0e6425652
gergesg <96778235+gergesg@users.noreply.github.com>
2025-10-29 23:05:33 +0300

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-const survey = new Survey.Model(json);
-survey.onComplete.add((sender, options) => {
-    console.log(JSON.stringify(sender.data, null, 3));
-});
-survey.render(document.getElementById("surveyElement"));
