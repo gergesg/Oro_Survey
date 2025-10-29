4cdbb1519f25cfe5832e9d242add0c2bcca604af
gergesg <96778235+gergesg@users.noreply.github.com>
2025-10-29 21:36:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,5 +8,13 @@ const root = ReactDOM.createRoot(document.getElementById("surveyCreatorContainer
 
 root.render(<SurveyCreatorRenderComponent />);
 
+            fetch("survey.json")
+  .then(response => response.json())
+  .then(surveyJSON => {
+    const survey = new Survey.Model(surveyJSON);
+    Survey.SurveyNG.render("surveyContainer", { model: survey });
+  })
+  .catch(error => console.error("Failed to load survey:", error));
+
 
 
