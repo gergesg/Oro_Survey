b803023d28320839a9651c07d20495142613b996
gergesg <96778235+gergesg@users.noreply.github.com>
2025-10-30 23:09:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const survey = new Survey.Model(json);
 survey.onComplete.add((sender, options) => {
-  fetch("https://script.google.com/macros/s/AKfycbx2EfzZmql4J6XCEOF4nTSLvcvSbk0Ob9ydu9kCMICgCBJNfIXoEAVr42b-Z9wCHB8rEA/exec", {
+  fetch("https://script.google.com/macros/s/AKfycbxroOwdyzn6o_dSQ4ciQXepgRF34peL7ogQStF5p5cGHvx396zsl8ITGm5hJUdunkz7uw/exec", {
     method: "POST",
     body: JSON.stringify(sender.data)
   });
 });
 survey.render(document.getElementById("surveyElement"));
+
 
